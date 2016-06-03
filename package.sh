7051165aac29d68a10a9e24fd6c09593d0b3f696
Nash Gadre <allnash@live.com>
2016-06-02 21:32:13 -0400

    Adding Package script to instal binary

--- package.sh
+++ package.sh
@@ -3,7 +3,16 @@ git fetch --all && git pull
 echo "+ APP READY +"
 echo "+ ATTEMPTING TO BUILD SNF SEARCH TOOL +"
 ./bin/activator compile
-echo "+ ATTEMPTING TO BUILD CURRENT .TAR FOR UNIVERSAL SYSTEMS +"
-./bin/activator universal:packageBin
+echo "+ ATTEMPTING TO BUILD CURRENT BINARY FOR UNIVERSAL SYSTEMS +"
+./bin/activator pack
 echo "+ STAGING BUILD COMPLETED +"
-cp target/universal/snfsearch-1.0.zip .
+echo "+ INSTALLING 'snf' TO YOUR  $ ~/local/bin/snf"
+cd target/pack; make install
+export PATH=$HOME/local/bin:$PATH
+echo " "
+echo "+ SUCCESSFULLY INSTALLED 'snf' , RUN PROGRAM BY TYPING 'snf' +"
+echo " "
+echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +"
+echo "+    Add the following configuration to your .bash_profile, .zsh_profile, etc. for the usability:         +"
+echo "+    export PATH= $ HOME/local/bin: $ PATH                                                                + "
+echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +"
